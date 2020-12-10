fab782cea97d171c9c8d20c9e8be3873c5c3281a
houx5 <70970170+houx5@users.noreply.github.com>
2020-12-11 07:03:14 +0800

    Update Point.java

--- Point.java
+++ Point.java
@@ -1,21 +1,13 @@
-public class Point { 
-	protected int x, y; //coordinates
-	
-	public Point( int x, int y ) {
-		setPoint( x,y );
-	}
-	public int getX() {
-		return x;
-	}
-	public int getY() {
-		return y; 
-	}
-	
-	
-	public double distanceTo(Point that){
-		double d = Math.pow(x-that.x,2) + Math.pow(y-that.y,2); 
-	   
-		return Math.sqrt(d);  
-	}
-		
+public class Point {
+    int x;
+    int y;
+    public Point(int a, int b){
+        x = a;
+        y = b;
+    }
+    public void Set(int a, int b){
+        x = a;
+        y = b;
+    }
+
 }
